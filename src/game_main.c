ca908b9b99a0eb6d634b04e799825fd3d81f8666
wayfarerGameDev <84999694+wayfarerGameDev@users.noreply.github.com>
2022-11-05 14:21:27 -0700

    24 - Define renderer_2d as renderer_2d_opengl

--- src/game_main.c
+++ src/game_main.c
@@ -2,7 +2,7 @@
 #include "calypso_framework_app_sdl.c"
 #include "calypso_framework_colors.c"
 #include "calypso_framework_input_sdl.c"
-#include "calypso_framework_renderer_2d.c"
+#include "calypso_framework_renderer_2d_opengl.c"
 #include "calypso_framework_systems.c"
 
 void log_printf(const char* log_msg, const Uint8 log_type)
@@ -27,14 +27,14 @@ void log_printf(const char* log_msg, const Uint8 log_type)
 void start(void)
 {   
     // Setup Renderer
-    calypso_framework_renderer_2d_set_log_callback(log_printf);
-    calypso_framework_renderer_2d_init(calypso_framework_app_sdl_get_open_gl_proc_address());
+    calypso_framework_renderer_2d_opengl_set_log_callback(log_printf);
+    calypso_framework_renderer_2d_opengl_init(calypso_framework_app_sdl_get_open_gl_proc_address());
 }
 
 void end(void)
 {
     // Stop Renderer
-    calypso_framework_renderer_2d_deinit();
+    calypso_framework_renderer_2d_opengl_deinit();
 
     // :(
     log_printf("Goodbye cruel world :(\n",3);
@@ -53,13 +53,13 @@ void update(void)
     calypso_framework_input_sdl_update();
 
     // Render Start
-    calypso_framework_renderer_2d_set_clear_color_by_byte_color_array(_c_calypso_framework_colors_color_byte_array_black); // Don't need to  do this every frame but why not
-    calypso_framework_renderer_2d_set_viewport(0,-300,viewport_width,viewport_height); // Don't need to do this every frame but why not
-    calypso_framework_renderer_2d_clear();
+    calypso_framework_renderer_2d_opengl_set_clear_color_by_byte_color_array(_c_calypso_framework_colors_color_byte_array_black); // Don't need to  do this every frame but why not
+    calypso_framework_renderer_2d_opengl_set_viewport(0,-300,viewport_width,viewport_height); // Don't need to do this every frame but why not
+    calypso_framework_renderer_2d_opengl_clear();
 
     // Render Entities
-    calypso_framework_renderer_2d_render_box(0,0,0,0);
-    calypso_framework_renderer_2d_render_triangle(0,0,0,0);
+    calypso_framework_renderer_2d_opengl_render_box(0,0,0,0);
+    calypso_framework_renderer_2d_opengl_render_triangle(0,0,0,0);
 
     // Render GUI Here
 }
@@ -68,7 +68,7 @@ int main(int argc, char** argv)
 {
     // Setup | Run App
     calypso_framework_app_sdl_set_log_callback(log_printf);
-    calypso_framework_app_sdl_init_window_with_opengl(CALYPSO_FRAMEWORK_RENDERER_GL_MAJOR_VERSION,CALYPSO_FRAMEWORK_RENDERER_GL_MINOR_VERSION,CALYPSO_FRAMEWORK_RENDERER_GL_CONTEXT_PROFILE);
+    calypso_framework_app_sdl_init_window_with_opengl(CALYPSO_FRAMEWORK_RENDERER_OPENGL_MAJOR_VERSION,CALYPSO_FRAMEWORK_RENDERER_OPENGL_MINOR_VERSION,CALYPSO_FRAMEWORK_RENDERER_OPENGL_CONTEXT_PROFILE);
     calypso_framework_app_sdl_set_window_title("Game");
     calypso_framework_app_sdl_set_events(start,end,update);
     calypso_framework_app_sdl_run();
