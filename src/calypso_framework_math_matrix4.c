52a01b1e0e58db703c56eca2663216f5073d793d
wayfarerGameDev <84999694+wayfarerGameDev@users.noreply.github.com>
2022-11-13 20:08:23 -0800

    32[Patch] - Clean up matrix code

--- src/calypso_framework_math_matrix4.c
+++ src/calypso_framework_math_matrix4.c
@@ -1,23 +1,24 @@
 /// <summary>
-/// Matrix Math
+/// Matrix Math.
+/// Uses Column Row Major Order Method
 /// </summary>
 
 #pragma once
 
 /**
-* \brief Caculate Ortho matrx4f
+* \brief Caculate Ortho matrx4f (Column Row Major Order)
 * \return void
 */
-void calypso_framework_math_matrix_ortho_matrix4f(float matrix4f[4][4], float left, float right, float bottom, float top, float near, float far)
+void calypso_framework_math_matrix_ortho_matrix4f(float matrix4f[4][4], const float left, const float right, const float bottom, const float top, const float zNear, const float zFar)
 {
 	matrix4f[0][0] = 2.f/(right-left);
 	matrix4f[0][1] = matrix4f[0][2] = matrix4f[0][3] = 0.f;
 	matrix4f[1][1] = 2.f/(top-bottom);
 	matrix4f[1][0] = matrix4f[1][2] = matrix4f[1][3] = 0.f;
-	matrix4f[2][2] = -2.f/(far-near);
+	matrix4f[2][2] = -2.f/(zFar-zNear);
 	matrix4f[2][0] = matrix4f[2][1] = matrix4f[2][3] = 0.f;
 	matrix4f[3][0] = -(right+left)/(right-left);
 	matrix4f[3][1] = -(top+bottom)/(top-bottom);
-	matrix4f[3][2] = -(far+near)/(far-near);
+	matrix4f[3][2] = -(zFar+zNear)/(zFar-zNear);
 	matrix4f[3][3] = 1.f;   
 }
