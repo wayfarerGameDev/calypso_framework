4aebe827b53ce2196b181665fce0f207581caef5
wayfarerGameDev <84999694+wayfarerGameDev@users.noreply.github.com>
2022-11-15 06:16:27 -0800

    33- Screenspace for both pixel and vector  coordinates

--- src/calypso_framework_math_matrix4.c
+++ src/calypso_framework_math_matrix4.c
@@ -1,24 +1,65 @@
 /// <summary>
-/// Matrix Math.
-/// Uses Column Row Major Order Method
+/// A Simple Matrix Math moudle that is 100% portable and independant as it does not use defined types.
+/// Uses column row major order method
 /// </summary>
 
 #pragma once
 
 /**
-* \brief Caculate Ortho matrx4f (Column Row Major Order)
+* \brief Build a view lookat matrix4f (Column Row Major Order)
 * \return void
 */
-void calypso_framework_math_matrix_ortho_matrix4f(float matrix4f[4][4], const float left, const float right, const float bottom, const float top, const float zNear, const float zFar)
+void calypso_framework_math_matrix_build_view_lookat_matrix4f(const float from_x, const float from_y, const float from_z, const float to_x, const float to_y, const float to_z, const float up_x, const float up_y, const float up_z, float matrix4f_out[4][4])
 {
-	matrix4f[0][0] = 2.f/(right-left);
-	matrix4f[0][1] = matrix4f[0][2] = matrix4f[0][3] = 0.f;
-	matrix4f[1][1] = 2.f/(top-bottom);
-	matrix4f[1][0] = matrix4f[1][2] = matrix4f[1][3] = 0.f;
-	matrix4f[2][2] = -2.f/(zFar-zNear);
-	matrix4f[2][0] = matrix4f[2][1] = matrix4f[2][3] = 0.f;
-	matrix4f[3][0] = -(right+left)/(right-left);
-	matrix4f[3][1] = -(top+bottom)/(top-bottom);
-	matrix4f[3][2] = -(zFar+zNear)/(zFar-zNear);
-	matrix4f[3][3] = 1.f;   
+}
+
+/**
+* \brief Build a projection ortho matrix4f (Column Row Major Order)
+* \return void
+*/
+void calypso_framework_math_matrix_build_projection_ortho_matrix4f(const float left, const float right, const float bottom, const float top, const float zNear, const float zFar, float matrix4f_out[4][4])
+{
+	matrix4f_out[0][0] = 2.f/(right-left);
+	matrix4f_out[0][1] = 0;
+	matrix4f_out[0][2] = 0;
+	matrix4f_out[0][3] = 0;
+	
+	matrix4f_out[1][0] = 0;
+	matrix4f_out[1][1] = 2.f/(top-bottom);
+	matrix4f_out[1][2] = 0;
+	matrix4f_out[1][3] = 0;
+
+	matrix4f_out[2][0] = 0;
+	matrix4f_out[2][1] = 0;
+	matrix4f_out[2][2] = -2.f/(zFar-zNear);
+	matrix4f_out[2][3] = 0;
+	
+	matrix4f_out[3][0] = -(right+left)/(right-left);
+	matrix4f_out[3][1] = -(top+bottom)/(top-bottom);
+	matrix4f_out[3][2] = -(zFar+zNear)/(zFar-zNear);
+	matrix4f_out[3][3] = 1;   
+}
+
+/**
+* \brief Multiplies two matrixs (Column Row Major Order)
+* \return void
+*/
+void calypso_framework_math_matrix_mult_matrix4f(const float matrix4f_a[4][4], const float matrix4f_b[4][4], float matrix4f_out[4][4])
+{
+	matrix4f_out[0][0] = matrix4f_a[0][0] * matrix4f_b[0][0];
+	matrix4f_out[0][1] = matrix4f_a[0][1] * matrix4f_b[0][1];
+	matrix4f_out[0][2] = matrix4f_a[0][2] * matrix4f_b[0][2];
+	matrix4f_out[0][3] = matrix4f_a[0][3] * matrix4f_b[0][3];
+	matrix4f_out[1][0] = matrix4f_a[1][0] * matrix4f_b[1][0];
+	matrix4f_out[1][1] = matrix4f_a[1][1] * matrix4f_b[1][1];
+	matrix4f_out[1][2] = matrix4f_a[1][2] * matrix4f_b[1][2];
+	matrix4f_out[1][3] = matrix4f_a[1][3] * matrix4f_b[1][3];
+	matrix4f_out[2][0] = matrix4f_a[2][0] * matrix4f_b[2][0];
+	matrix4f_out[2][1] = matrix4f_a[2][1] * matrix4f_b[2][1];
+	matrix4f_out[2][2] = matrix4f_a[2][2] * matrix4f_b[2][2];
+	matrix4f_out[2][3] = matrix4f_a[2][3] * matrix4f_b[2][3];
+	matrix4f_out[3][0] = matrix4f_a[3][0] * matrix4f_b[3][0];
+	matrix4f_out[3][1] = matrix4f_a[3][1] * matrix4f_b[3][1];
+	matrix4f_out[3][2] = matrix4f_a[3][2] * matrix4f_b[3][2];
+	matrix4f_out[3][3] = matrix4f_a[3][3] * matrix4f_b[3][3];
 }
--- src/game_main.c
+++ src/game_main.c
@@ -4,11 +4,22 @@
 #include "calypso_framework_input_sdl.c"
 #include "calypso_framework_io.c"
 #include "calypso_framework_math_matrix4.c"
-#include "calypso_framework_renderer_2d_opengl.c"
+#include "calypso_framework_renderer_pixel_opengl.c"
 #include "calypso_framework_systems.c"
 
-float game_camera_projection_matrix[4][4];
-float game_camera_view_matrix[4][4];
+//Game Types
+typedef float game_matrix4f_t[4][4];
+typedef float game_vec4f_t[4];
+
+// Camera
+game_matrix4f_t game_camera_projection_matrix;
+game_matrix4f_t game_camera_view_matrix;
+game_matrix4f_t game_camera_projection_view_matrix;
+
+//Model
+game_matrix4f_t game_model_matrix;
+
+//Shaders
 unsigned int game_default_shader_program_red;
 unsigned int game_default_shader_program_yellow;
 
@@ -36,32 +47,43 @@ void start(void)
     // Setup IO
     calypso_framework_io_set_log_callback(log_printf);
 
+    const float viewport_width = calypso_framework_app_sdl_get_window_width();
+    const float viewport_height = calypso_framework_app_sdl_get_window_height();
+
     // Renderer
     {   
-        //Set Renderer Log Callback | Init Renderer
-        calypso_framework_renderer_2d_opengl_set_log_callback(log_printf);
-        calypso_framework_renderer_2d_opengl_init(calypso_framework_app_sdl_get_open_gl_proc_address());
-
-        //Camera
-        calypso_framework_math_matrix_ortho_matrix4f(game_camera_projection_matrix,-8,8,-4.5f,4.5f,-1,1);
+        //Camera (Every Time camera Updates)
+        calypso_framework_math_matrix_build_projection_ortho_matrix4f(0,viewport_width,0,viewport_height,-1,1,game_camera_projection_matrix); // Pixel Screen Space
+        //calypso_framework_math_matrix_build_projection_ortho_matrix4f(-8,8,-4.5f,4.5f,-1,1,game_camera_projection_matrix); // Vector Screen Space
+        calypso_framework_math_matrix_build_view_lookat_matrix4f(0,0,-10,0,0,0,0,1,0,game_camera_view_matrix);
         
-        // Create Default Shader Program Red
-        game_default_shader_program_red = calypso_framework_renderer_2d_opengl_create_default_shader_program();
-        calypso_framework_renderer_2d_opengl_set_current_render_shader_program(game_default_shader_program_red);
-        calypso_framework_renderer_2d_opengl_set_current_shader_program_parameter_vec4f("color_in",1,0,0,1);
-        calypso_framework_renderer_2d_opengl_set_current_shader_program_parameter_matrix4f("mvp_in",game_camera_projection_matrix); // Apply Camera
-        // Create Default Shader Program Yellow
+        //calypso_framework_math_matrix_mult_matrix4f(game_camera_projection_matrix,game_camera_view_matrix,game_camera_projection_view_matrix);s
 
-        //game_default_shader_program_yellow = calypso_framework_renderer_2d_opengl_create_default_shader_program();
-        //calypso_framework_renderer_2d_opengl_set_current_render_shader_program(game_default_shader_program_yellow);
-        //calypso_framework_renderer_2d_opengl_set_current_shader_program_parameter_vec4f("color_in",1,1,0,1);
+        //Set Renderer Log Callback | Init Renderer
+        calypso_framework_renderer_pixel_opengl_set_log_callback(log_printf);
+        calypso_framework_renderer_pixel_opengl_init(calypso_framework_app_sdl_get_open_gl_proc_address());
+        calypso_framework_renderer_pixel_opengl_set_screen_space_coordinates(1);
+
+        //Create Default Shader Program Red
+        game_default_shader_program_red = calypso_framework_renderer_pixel_opengl_create_default_shader_program();
+        calypso_framework_renderer_pixel_opengl_set_current_render_shader_program(game_default_shader_program_red);
+        calypso_framework_renderer_pixel_opengl_set_current_shader_program_parameter_vec4f("color_in",1,0,0,1);
+        calypso_framework_renderer_pixel_opengl_set_current_shader_program_parameter_matrix4f("model_in",game_model_matrix); // Apply Transform
+        calypso_framework_renderer_pixel_opengl_set_current_shader_program_parameter_matrix4f("projectionView_in",game_camera_projection_matrix); // Apply Camera
+       
+        //Create Default Shader Program Yellow
+        game_default_shader_program_yellow = calypso_framework_renderer_pixel_opengl_create_default_shader_program();
+        calypso_framework_renderer_pixel_opengl_set_current_render_shader_program(game_default_shader_program_yellow);
+        calypso_framework_renderer_pixel_opengl_set_current_shader_program_parameter_vec4f("color_in",1,1,0,1);
+        calypso_framework_renderer_pixel_opengl_set_current_shader_program_parameter_matrix4f("model_in",game_model_matrix); // Transform
+        calypso_framework_renderer_pixel_opengl_set_current_shader_program_parameter_matrix4f("projectionView_in",game_camera_projection_matrix); // Apply Camera
     }
 }
 
 void end(void)
 {
     // Stop Renderer
-    calypso_framework_renderer_2d_opengl_deinit();
+    calypso_framework_renderer_pixel_opengl_deinit();
 
     // :(
     log_printf("Goodbye cruel world :(\n",3);
@@ -78,15 +100,14 @@ void update(void)
     calypso_framework_input_sdl_update();
 
     // Render Start
-    calypso_framework_renderer_2d_opengl_set_clear_color_by_byte_color_array(_c_calypso_framework_colors_color_byte_array_black); // Don't need to  do this every frame but why not
-    calypso_framework_renderer_2d_opengl_clear();
+    calypso_framework_renderer_pixel_opengl_set_clear_color_by_byte_color_array(_c_calypso_framework_colors_color_byte_array_black); // Don't need to  do this every frame but why not
+    calypso_framework_renderer_pixel_opengl_clear();
 
     // Render Entities
-    calypso_framework_renderer_2d_opengl_set_current_render_shader_program(game_default_shader_program_red);
-    calypso_framework_renderer_2d_opengl_render_box(0,0,0,0);
-
-    //calypso_framework_renderer_2d_opengl_set_current_render_shader_program(game_default_shader_program_yellow);
-    //calypso_framework_renderer_2d_opengl_render_triangle(0,0,0,0);
+    calypso_framework_renderer_pixel_opengl_set_current_render_shader_program(game_default_shader_program_red);
+    calypso_framework_renderer_pixel_opengl_render_box(0,0,0,0);
+    calypso_framework_renderer_pixel_opengl_set_current_render_shader_program(game_default_shader_program_yellow);
+    calypso_framework_renderer_pixel_opengl_render_box(0,0,0,0);
 
     // Render GUI Here
 }
@@ -95,7 +116,7 @@ int main(int argc, char** argv)
 {
     // Setup | Run App
     calypso_framework_app_sdl_set_log_callback(log_printf);
-    calypso_framework_app_sdl_init_with_opengl(CALYPSO_FRAMEWORK_RENDERER_OPENGL_MAJOR_VERSION,CALYPSO_FRAMEWORK_RENDERER_OPENGL_MINOR_VERSION,CALYPSO_FRAMEWORK_RENDERER_OPENGL_CONTEXT_PROFILE);
+    calypso_framework_app_sdl_init_with_opengl(CALYPSO_FRAMEWORK_RENDERER_PIXEL_OPENGL_MAJOR_VERSION,CALYPSO_FRAMEWORK_RENDERER_PIXEL_OPENGL_MINOR_VERSION,CALYPSO_FRAMEWORK_RENDERER_PIXEL_OPENGL_CONTEXT_PROFILE);
     calypso_framework_app_sdl_set_window_title("Game");
     calypso_framework_app_sdl_set_events(start,end,update);
     calypso_framework_app_sdl_run();
